1b42d41a33b739ce1d8ffccda6063c6086864311
ZenJL <minhle7206@gmail.com>
2021-10-16 14:18:04 +0700

    update Project

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,6 +4,11 @@ const backendApi = 'https://tony-json-server.herokuapp.com';
 const form = document.getElementById('login-form');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
+const loading = document.getElementById('loading');
+// loading.style.display = 'none';
+
+
+let usersList = [];
 
 function getUsers(callback) {
     fetch(`${backendApi}/api/users`)
@@ -11,34 +16,70 @@ function getUsers(callback) {
         .then(callback)
 };
 
+getUsers(responseData => usersList = responseData.data);
+
 form.addEventListener('submit', function(event) {
+    loading.style.display = 'block';
     event.preventDefault();
     checkLogin();
+    
 });
 
 function checkLogin() {
-    getUsers(responseData => {
-        const usersList = responseData.data;
 
-        const emailValue = emailInput.value;
-        const passwordValue = passwordInput.value;
-        // console.log('u need: ', emailValue)
-        // console.log('u need: ', passwordValue)
+    const emailValue = emailInput.value;
+    const passwordValue = passwordInput.value;
+    // console.log('u need: ', emailValue)
+    // console.log('u need: ', passwordValue)
 
-        console.log(usersList);
+    setTimeout(() => {
+        loading.style.display = 'none';
+    }, 1500);
 
-        const existUser = usersList.some(user => user.email === emailValue && user.password === passwordValue);
-        // if(user.email === emailValue && user.password === passwordValue) {
-        //     window.location.href = '../../registerPage.html'
-        // }
-        if(existUser === true) {
-            window.location.href = '../../index.html'
-        }
-        else {
-            alert('something wrong or missing. Please check your email and password again!')
-        }
+    console.log(usersList);
+
+    const existUser = usersList.filter(user => {
+        return user.email === emailValue;
     });
+    console.log('u need: ', existUser);
+
+    // invalid email
+    if(existUser.length > 0) {
+        if(existUser[0].password === passwordValue) {
+            loading.classList.add('active');
+            localStorage.setItem('user', email.value);
+            if (localStorage.getItem('user') !== null) {
+                setTimeout(() => {
+                    window.location.href = '../../index.html'
+                }, 1500);
+            };
+        };
+
+        if(existUser[0].password !== passwordValue) {
+            const showPassError = document.getElementById('password-error-mess');
+            passwordInput.style.border = "1px solid red";
+            showPassError.style.display = "block";
+            showPassError.innerHTML = "The password incorrect. Please check your password again.";
+        };
+
+    }
+    else {
+        const showEmailError = document.getElementById('email-error-mess');
+        emailInput.style.border = "1px solid red";
+        showEmailError.style.display = "block";
+        showEmailError.innerHTML = "The email hasn't been registered yet. Please check your email and try again.";
+    };
+
+
+    // const existUser = usersList.filter(user => user.email === emailValue && user.password === passwordValue);
+    // if(user.email === emailValue && user.password === passwordValue) {
+    //     window.location.href = '../../registerPage.html'
+    // }
+    // if(existUser === true) {
+    //     window.location.href = '../../index.html'
+    // }
+    // if()
 };
 
-// checkLogin();
 
+//Password incorrect. Please check your password again.
